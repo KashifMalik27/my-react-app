b6e2115bed0afa9ca93c1e942e3a79e4f230eb88
KashifMalik27 <malikkashif2700@gmail.com>
2026-09-16 16:42:02 +0500

    feat: workflow add kia ha

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
 <main class="home">
   <section class="hero-section" aria-labelledby="page-title">
-    <p class="eyebrow">WELCOME HOME</p>
+    <p class="eyebrow">WELCOME HOME page</p>
     <h1 id="page-title">My First App</h1>
   </section>
 </main>
